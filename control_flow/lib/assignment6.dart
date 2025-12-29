d7981502739e539749b16746f4797b116453f49d
Shemanto Saha <shemanto.saha.07d3@gmail.com>
2025-12-29 23:16:40 +0600

    Add files via upload

---
+++ control_flow/lib/assignment6.dart
@@ -0,0 +1,14 @@
+/*
+Assignment 6 - Print numbers from 1 to 10 using while loop
+  - Understand loop initialization
+  - Learn how conditions control repetition
+  - Avoid infinite loop
+*/
+
+void main() {
+  int i = 1;
+  while (i <= 10) {
+    print(i);
+    i++;
+  }
+}
